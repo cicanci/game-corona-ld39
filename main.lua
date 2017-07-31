72dfcbef6515109673d32fc0fec6943d15803629
Bruno Cicanci <brunocicanci@gmail.com>
2017-07-30 21:45:29 -0300

    Player horizontal movement

--- main.lua
+++ main.lua
@@ -4,4 +4,34 @@
 --
 -----------------------------------------------------------------------------------------
 
--- Your code here
+display.setStatusBar(display.HiddenStatusBar)
+
+local group = display.newGroup()
+
+local player = display.newImage("content/player.png")
+player.x = display.contentWidth * 0.5
+player.y = display.contentHeight * 0.5
+group:insert(player)
+
+local movement = 0
+local speed = 10
+
+local function movePlayer(event)
+  player.x = player.x + movement
+end
+Runtime:addEventListener("enterFrame", movePlayer)
+
+local function onTouchScreen(event)
+  if (event.phase == "began") then
+    if (event.x > display.contentCenterX) then
+      movement = speed
+      player.xScale = -1
+    else
+      movement = -speed
+      player.xScale = 1
+    end
+  elseif (event.phase == "ended") then
+    movement = 0
+  end
+end
+Runtime:addEventListener("touch", onTouchScreen)
